cfdf7e4abd4c8d0d2b13ae62d7cfe2b2a2526eba
YuriyIzbash <izbash91@gmail.com>
2024-12-01 19:35:51 +0100

    Added performLocalSearch func

--- MapsDirections/Main/MainController.swift
+++ MapsDirections/Main/MainController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import MapKit
 import LBTATools
+import SwiftUI
 
 extension MainController: MKMapViewDelegate {
     
@@ -33,15 +34,15 @@ class MainController: UIViewController {
         
         mapView.fillSuperview()
         
-        setupRegionForMap()
+//        setupAnnotationsForMap()
         
-        setupAnnotationsForMap()
+        performLocalSearch()
         
     }
     
     fileprivate func setupRegionForMap() {
         let centerCoordinate = CLLocationCoordinate2D(latitude: 37.7666, longitude: -122.427290)
-        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
         let region = MKCoordinateRegion(center: centerCoordinate, span: span)
         mapView.setRegion(region, animated: true)
     }
@@ -61,26 +62,71 @@ class MainController: UIViewController {
         
         mapView.showAnnotations(self.mapView.annotations, animated: true)
     }
-}
-
-// SwiftUI Preview
-import SwiftUI
-
-struct MainPreview: PreviewProvider {
-    static var previews: some View {
-        ContainerView()
-            .edgesIgnoringSafeArea(.all)
+    
+    fileprivate func performLocalSearch() {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Apple"
+        request.region = mapView.region
+        
+        let localSearch = MKLocalSearch(request: request)
+        localSearch.start { (resp, err) in
+            // Failure
+            if let err = err {
+                print("Failed local search. Error:", err)
+                return
+            }
+            // Success
+            resp?.mapItems.forEach({ (mapItem) in
+//                print(mapItem.placemark.subThoroughfare ?? "")
+                let placemark = mapItem.placemark
+                var addrressString = ""
+                if placemark.subThoroughfare != nil {
+                    addrressString = placemark.subThoroughfare! + " "
+                }
+                if placemark.thoroughfare != nil {
+                    addrressString += placemark.thoroughfare! + ", "
+                }
+                if placemark.postalCode != nil {
+                    addrressString += placemark.postalCode! + " "
+                }
+                if placemark.locality != nil {
+                    addrressString += placemark.locality! + ", "
+                }
+                if placemark.administrativeArea != nil {
+                    addrressString += placemark.administrativeArea! + " "
+                }
+                if placemark.country != nil {
+                    addrressString += placemark.country!
+                }
+                print(addrressString)
+                
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = mapItem.placemark.coordinate
+                annotation.title = mapItem.name
+                self.mapView.addAnnotation(annotation)
+            })
+            self.mapView.showAnnotations(self.mapView.annotations, animated: true)
+        }
     }
     
-    struct ContainerView: UIViewControllerRepresentable {
-        func makeUIViewController(context: Context) -> MainController {
-            return MainController()
+    // SwiftUI Preview
+    
+    struct MainPreview: PreviewProvider {
+        static var previews: some View {
+            ContainerView()
+                .edgesIgnoringSafeArea(.all)
         }
         
-        func updateUIViewController(_ uiViewController: MainController, context: Context) {
+        struct ContainerView: UIViewControllerRepresentable {
+            func makeUIViewController(context: Context) -> MainController {
+                return MainController()
+            }
             
+            func updateUIViewController(_ uiViewController: MainController, context: Context) {
+                
+            }
+            
+            typealias UIViewControllerType = MainController
         }
-        
-        typealias UIViewControllerType = MainController
     }
 }
